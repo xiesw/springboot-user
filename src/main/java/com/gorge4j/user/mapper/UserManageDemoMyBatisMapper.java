5fd019f894af16abf21fddc2d77f5ecc393fccf0
pain.xie <pain.xie@wolaidai.com>
2020-01-06 15:25:10 +0800

    6.2.1 init本地运行

--- src/main/java/com/gorge4j/user/mapper/UserManageDemoMyBatisMapper.java
+++ src/main/java/com/gorge4j/user/mapper/UserManageDemoMyBatisMapper.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import com.gorge4j.user.entity.UserManageDemoMyBatis;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * @Title: UserManageDemoMyBatisMapper.java
@@ -21,7 +22,7 @@ public interface UserManageDemoMyBatisMapper {
 
     /**
      * 用户注册/新增
-     * 
+     *
      * @param userManageDemoMyBatis
      * @return
      */
@@ -30,37 +31,37 @@ public interface UserManageDemoMyBatisMapper {
 
     /**
      * 通过名字、密码和类型查询用户信息
-     * 
+     *
      * @param name 用户名
      * @param password 用户密码
      * @param type 用户类型
      * @return
      */
-    UserManageDemoMyBatis findUserManageDemoByNameAndPasswordAndType(String name, String password, String type);
+    UserManageDemoMyBatis findUserManageDemoByNameAndPasswordAndType(@Param("name") String name, @Param("password") String password,@Param("type") String type);
 
     /**
      * 通过类型、状态查询用户信息
-     * 
+     *
      * @param type 用户类型
      * @param isDelete 用户状态
      * @return
      */
-    List<UserManageDemoMyBatis> findAllByTypeAndStatus(String type, Boolean isDelete);
+    List<UserManageDemoMyBatis> findAllByTypeAndStatus(@Param("type") String type,@Param("isDelete") Boolean isDelete);
 
     /**
      * 通过名字和类型更新用户密码
-     * 
+     *
      * @param password 用户密码
      * @param name 用户名
      * @param type 用户类型
      * @param gmtModified 更新时间
      * @return
      */
-    int updatePasswordByNameAndType(String password, String name, String type, Date gmtModified);
-    
+    int updatePasswordByNameAndType(@Param("password") String password,@Param("name") String name,@Param("type") String type,@Param("gmtModified") Date gmtModified);
+
     /**
      * 更新用户信息
-     * 
+     *
      * @param userManageDemoMyBatis
      * @return
      */
@@ -68,20 +69,20 @@ public interface UserManageDemoMyBatisMapper {
 
     /**
      * 通过用户id更新用户状态
-     * 
+     *
      * @param id 用户id
      * @param gmtModified 更新时间
      * @return
      */
-    int updateStatusById(Integer id, Date gmtModified);
+    int updateStatusById(@Param("id") Integer id,@Param("gmtModified") Date gmtModified);
 
     /**
      * 通过用户名和类型查找用户
-     * 
+     *
      * @param name 用户名
      * @param type 用户类型
      * @return
      */
-    UserManageDemoMyBatis findUserManageDemoByNameAndType(String name, String type);
+    UserManageDemoMyBatis findUserManageDemoByNameAndType(@Param("name") String name, @Param("type") String type);
 
 }
--- src/main/java/com/gorge4j/user/servlet/DruidStatViewServlet.java
+++ src/main/java/com/gorge4j/user/servlet/DruidStatViewServlet.java
@@ -18,7 +18,7 @@ import com.alibaba.druid.support.http.StatViewServlet;
 @WebServlet(urlPatterns = "/druid/*", initParams = {@WebInitParam(name = "allow", value = "127.0.0.1"), // IP白名单
                                                                                                         // (没有配置或者为空，则允许所有访问)
         @WebInitParam(name = "loginUsername", value = "root"), // 用户名
-        @WebInitParam(name = "loginPassword", value = "12345678"), // 密码
+        @WebInitParam(name = "loginPassword", value = "123456"), // 密码
         @WebInitParam(name = "resetEnable", value = "false") // 禁用HTML页面上的“Reset All”功能
 })
 public class DruidStatViewServlet extends StatViewServlet {
